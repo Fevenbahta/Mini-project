5202c9ba036d6c0071f6932d1900a699263dd646
FevenB <FevenB@ITS-PC1-206.LIB5.COM>
2025-02-17 09:52:21 +0300

    miniproject-first-commit

---
+++ LIB.DATA.Persistence/PersistenceServiceRegistrtion.cs
@@ -0,0 +1,39 @@
+﻿
+using LIB.API.Persistence.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PERSISTANCE.Services;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+
+
+namespace LIB.API.Persistence
+{
+    public static partial class PersistenceServiceRegistrtion
+    {
+        public static IServiceCollection ConfigurePersistenceService(this IServiceCollection services, IConfiguration configuration)
+        {
+    
+            services.AddScoped<JwtService>();
+
+     
+
+            services.AddHttpClient();
+
+
+            services.AddHostedService<TcpServerService>();
+
+            return services;
+        }
+    }
+}
